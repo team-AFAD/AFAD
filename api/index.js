6f0812fc0e65b8bee4030cdb9cadf32f498217cf
junghwaK <jeonghwa054@gmail.com>
2022-10-15 15:31:10 +0900

    connection: mongoDB

---
+++ api/index.js
@@ -0,0 +1,31 @@
+//main (아래 둘중 하나로 설정하면됨)
+import express from "express"
+// const express = require("express")
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+const app = express()
+dotenv.config()
+
+//env.MONGO와 연결
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connect to mongoDB")
+    } catch(error) {
+        throw error
+    }
+};
+
+mongoose.connection.on("disconnected", ()=> {
+    console.log("mongoDB disconnected")
+})
+
+mongoose.connection.on("connected", ()=> {
+    console.log("mongoDB connected")
+})
+
+//8080연결완료
+app.listen(8080, ()=>{
+    connect()
+    console.log("Connected to backend!")
+})
