0735dac3a8f1414b2f35213493ef287c306bc290
junghwaK <jeonghwa054@gmail.com>
2022-10-15 18:17:12 +0900

    feat: register and check save data

---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    profilePicture:{
+        type:String,
+        default:""
+    },
+    city: {
+        type: String,
+        max: 50,
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+},{timestamps : true});
+
+export default mongoose.model("User", UserSchema);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,18 @@
 import express from "express";
+import { get } from "http";
+import User from "../models/User.js";
 
 const router = express.Router();
+
+router.get("")
+
+//UPDATE
+
+//DELETE
+
+//GET
+
+//GET ALL
 
 
 
