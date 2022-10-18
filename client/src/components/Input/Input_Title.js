22fe6c95f7685fcafde0d1103c8a421a0b2b6228
Seoyoung-Mun <tjdud330@gmail.com>
2022-10-18 14:16:07 +0900

    feat : input component

--- client/src/components/Input/Input_Title.js
+++ client/src/components/Input/Input_Title.js
@@ -1,9 +1,14 @@
-function Input_Title (){
-    return(
-        <div className="Input_Title">
-            
+import React from 'react';
+import './input_title.scss';
+
+const Input_Title = ({title, name,type, onChangeForm}) => {
+    return (
+        <div className="Input_TitleWrap">
+            <div className="title">{title}</div>
+            <input type={type} name={name} onChange={onChangeForm}></input>
         </div>
     )
 }
 
 export default Input_Title;
+
--- client/src/components/Post/PostForm.js
+++ client/src/components/Post/PostForm.js
@@ -1,11 +1,35 @@
+import React, { useState } from 'react';
 import './postForm.scss';
-import InputTitle from '../Input/InputTitle';
+import InputTitle from '../Input/Input_Title';
 
 function PostForm () {
+    const [formValue, setFormValue] = useState({
+        title : ""
+        ,merchandiseName : ""
+      });
+
+      const onChangeForm = (e) => {
+        setFormValue({
+          ...formValue,
+          [e.target.name]: e.target.value,
+        });
+        console.log(formValue.title);
+        console.log(formValue.merchandiseName);
+      };
+    
     return (
         <div className="PostForm">
             <form>
-                <p>제목</p> <InputTitle/>
+                <InputTitle title={"제목"} name={"title"} type={"text"}
+                onChangeForm={onChangeForm}
+                />
+                <InputTitle title={"상품명"} name={"merchandiseName"} type={"text"}
+                onChangeForm={onChangeForm}
+                />
+                <InputTitle title={"상품명"} name={"merchandiseName"} type={"text"}
+                onChangeForm={onChangeForm}
+                />
+
             </form>
         </div>
     )
