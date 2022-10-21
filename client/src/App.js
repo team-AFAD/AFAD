15d252b061e1ebf95eb8d3dd940c60389b9c06a4
leeesurim <108252916+leeesurim@users.noreply.github.com>
2022-10-21 18:57:39 +0900

    feat: register

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 // import PostWrite from "./components/Post/PostWrite";
 import PostView from "./components/Read/PostView";
 
+import './app.scss';
+import RegisterForm from "./components/Register/RegisterForm";
 function App() {
   return (
     <div className="App">
+
       {/* <PostWrite /> */}
-      <PostView />
+      {/* <PostView /> */}
+
+      <RegisterForm />
     </div>
   );
 }
--- client/src/components/Register/RegisterForm.js
+++ client/src/components/Register/RegisterForm.js
@@ -1,38 +1,106 @@
 import React, { useState } from 'react';
 import InputRegister from "../Input/InputRegister";
-
 import './registerForm.scss';
+import InputPostFile from '../Input/InputPostFile';
 
 function RegisterForm () {
-    const [formValue, setFormValue] = useState({
-        email : "",
-        password : "",
-        passwordCheck : "",
-        username : "",
-        nickname : "",
-        profilePicture : ""
-    });
+    const [values, setValues] = useState({
+        email:"",
+        password:"",
+        confirmPassword:"",
+        username:"",
+        nickname:"",
+      });
 
-    const onChangeForm = (e) => {
-        setFormValue({
-        ...formValue,
-        [e.target.name]: e.target.value,
-        });
-    };
+    const inputs = [
+    {
+        id: 1,
+        name: "email",
+        type: "email",
+        placeholder: "이메일 주소를 입력해 주세요.",
+        errorMessage: "이메일 형식이 올바르지 않습니다. 다시 입력해 주세요.",
+        label:"이메일",
+        required: true,
+    },
+    {
+        id: 2,
+        name: "password",
+        type: "password",
+        placeholder: "8~20자의 영문,숫자,특수문자(!@#$%^&*) 조합",
+        errorMessage:
+          "8~20자로 문자/숫자/특수문자(!@#$%^&*)가 적어도 1개 이상을 포함해 주세요.",
+        label:"비밀번호",
+        pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+        required: true,
+      },
+    {
+        id: 3,
+        name: "confirmPassword",
+        type: "password",
+        placeholder: "비밀번호를 한번 더 입력해 주세요.",
+        errorMessage: "비밀번호가 일치하지 않습니다. 다시 입력해 주세요.",
+        label:"비밀번호 확인",
+        pattern: values.password,
+        required: true,
+    },
+    {
+        id: 4,
+        name: "username",
+        type: "text",
+        placeholder: "이름을 입력해 주세요.",
+        label:"이름",
+        required: true,
+    },
+    {
+        id: 5,
+        name: "nickname",
+        type: "text",
+        placeholder: "닉네임을 입력해 주세요.",
+        label:"닉네임",
+        required: true,
+    },
+
+    ]
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // const data = new FormData(e.target);
+        console.log(Object.fromEntries(formData(e.target).entries()));
+        console.log(values);
+      }
+
+    const onChange = (e) =>{
+        setValues({...values, [e.target.name]: e.target.value })
+    }
+
+    let formData = new FormData();
+
+    const onChangeFile = () => {
+        let fileUpload = document.querySelector(".InputPostFile input");
+        console.log(fileUpload);
+        formData.append("userfile", fileUpload.files[0]);
+    }
+
+    console.log(values);
     return(
         <div className="RegisterForm">
-            <InputRegister title={"이메일"} type={"text"} id={"email"} name={"email"} required
-            onChangeForm={onChangeForm} />
-            <InputRegister title={"비밀번호"} type={"password"} id={"password"} name={"password"} required
-            onChangeForm={onChangeForm} />
-            <InputRegister title={"비밀번호 확인"} type={"password"} id={"passwordCheck"} name={"passwordCheck"} required 
-            onChangeForm={onChangeForm} />
-            <InputRegister title={"이름"} type={"email"} id={"username"} name={"username"} required 
-            onChangeForm={onChangeForm} />
-            <InputRegister title={"닉네임"} type={"text"} id={"nickname"} name={"nickname"} required 
-            onChangeForm={onChangeForm} />
 
-            
+            <form onSubmit={handleSubmit}>
+                <h2>회원가입</h2>
+
+                {inputs.map((input) =>(
+                <InputRegister 
+                    key={input.id} 
+                    {...input} 
+                    values={values[input.name]}
+                    onChange={onChange}  
+                />
+                ) )}
+                <InputPostFile title={"프로필 사진"} name={"profilePicture"} type={"file"}
+                functionName={onChangeFile}
+                />
+            <button >Submit</button>
+            </form>
         </div>
         
     );
