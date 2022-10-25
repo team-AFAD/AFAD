44642bd1fe2c51c41712fe9b1df2d161f5aaf912
junghwaK <jeonghwa054@gmail.com>
2022-10-26 03:09:15 +0900

    feat: try to get a user info

--- socket/index.js
+++ socket/index.js
@@ -1,10 +1,17 @@
-// const { socket } = require("socket.io");
+const { socket } = require("socket.io");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http");
+app.use(cors());
 
-// const io = require("socket.io")(8000, {
-//     cors:{
-//         origin:"http://localhost:3000"
-//     }
-// });
+const io = require("socket.io")(8080, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+
+
 
 io.on("connection", (socket) => {
     console.log("user connected.")
