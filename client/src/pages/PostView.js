fef2310c219d4cd7df86ed42455e35b1879592a5
Seoyoung-Mun <tjdud330@gmail.com>
2022-10-26 00:41:22 +0900

    feat : axios.get PostView

--- client/src/pages/PostView.js
+++ client/src/pages/PostView.js
@@ -3,19 +3,36 @@ import MainInfo from '../components/Read/MainInfo';
 import Description from '../components/Read/Description';
 import Comment from '../components/Read/Comment';
 import '../components/Read/postView.scss'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 // import '../postView.scss';
 
-function PostView (){
+function PostView ( ){
+    const [data, setData] = useState([]);
+    const {id} = useParams();
 
-    const checkCookie = () => {
-
+    const getData = async () => {
+        const response = await axios.get(`http://localhost:8080/post:${id}`, {
+            params: { 
+                post : data.id ,
+            }
+        });
+        setData(response.data);
     }
+    useEffect(() => {
+        getData();
+    }, [])
+
+    // const checkCookie = () => {
+
+    // }
     
     return(
         <div className='PostView'>
             <div className='CompoWrap_flex'>
-                <PostImg />
-                <MainInfo />
+                <PostImg data={data.poto}/>
+                <MainInfo data={data.title}/>
             </div>
             
             <div className='postViewWrap'>
