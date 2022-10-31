9153fd1fd78d4c72725d57b98cc85bcd42cb8c61
Seoyoung-Mun <tjdud330@gmail.com>
2022-10-31 11:14:56 +0900

    feat : delete post

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -45,20 +45,21 @@ export const updatePost = async (req, res, next) =>{
 //DELETE
 export const deletePost = async (req, res, next) =>{
     try {
-        const post = await Post.findById(req.params.id);
-        if (post.nickname === req.body.nickname) {
-          try {
-            await post.delete();
-            res.status(200).json("Post has been deleted...");
-          } catch (err) {
-            res.status(500).json(err);
-          }
-        } else {
-          res.status(401).json("You can delete only your post!");
+      const post = await Post.findById(req.params.id);
+      console.log(post);
+      // if (post.nickname === req.body.nickname) {
+        try {
+          await post.delete();
+          res.status(200).json("Post has been deleted...");
+        } catch (err) {
+          res.status(500).json(err);
         }
-      } catch (err) {
-        res.status(500).json(err);
-      }
+      // } else {
+        // res.status(401).json("You can delete only your post!");
+      // }
+    } catch (err) {
+      res.status(500).json(err);
+    }
 };
 
 //GET(find)
